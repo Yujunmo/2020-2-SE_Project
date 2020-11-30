db1108a88cb947267484801062d2f3473f2b72b1
suhyun <rhan0871@naver.com>
2020-11-30 14:27:19 +0900

    ;

--- server/routes/removeWorker.js
+++ server/routes/removeWorker.js
@@ -6,10 +6,12 @@ const con=require("./database");
 router.get('/',async(req,res)=>{
    try{
       const userEmail=req.query.userEmail;
-
-      const sql=`delete from user where userEmail='${userEmail}'`;
+     
+      const sql=`delete from workhour where user_userEmail='${userEmail}'`;
+      const sql2=`delete from user where userEmail='${userEmail}'`;
 
       const [rows]=await con.query(sql);
+      const [rows2]=await con.query(sql2);
 
       return res.status(200).json({success:true});
    }catch(err){
